3d1543a03b6d9b06a427bc733942fff9cc3f2a44
Caroline Yu <carolineyu@Carolines-MBP.askey.com>
2023-01-30 14:44:23 -0500

    added animate get rid of scroll.js

--- README.md
+++ README.md
@@ -1,11 +1,4 @@
-# portfolio
+# PortFolio_Website
+Complete Portfolio Website with Bootstrap - HTML/CSS In this project, we are going to learn and build how to create a complete portfolio website with bootstrap using HTML and CSS. We will understand everything from scratch. 
 
-Your name
-Where you are from
-Your major
-Two to three sentences describing your web development background
-An image that represents your favorite place
-A link to your favorite restaurant
-A form that asks the user to subscribe to your newsletter – it should have a place to 
-enter first and last name, email, and a checkbox that acknowledges permission to be 
-added to the list.  You do not have to perform any validation – just present the form
+## To execute this project just open index.html file in the browser.
--- js/main.js
+++ js/main.js
@@ -1,26 +1,32 @@
-$(document).ready(function(){
-    let $btns= $('.project-area .button-group button');
-    $btns.click(function(e){
-        $('.project-area .button-group buttons').removeClass('active');
-        e.target.classList.add('active')
+$(document).ready(function () {
+
+
+    let $btns = $('.project-area .button-group button');
+
+
+    $btns.click(function (e) {
+
+        $('.project-area .button-group button').removeClass('active');
+        e.target.classList.add('active');
 
         let selector = $(e.target).attr('data-filter');
         $('.project-area .grid').isotope({
-            filter : selector
-        })
+            filter: selector
+        });
 
         return false;
     })
-
 
     $('.project-area .button-group #btn1').trigger('click');
 
     $('.project-area .grid .test-popup-link').magnificPopup({
         type: 'image',
-        gallery:{enabled:true}
+        gallery: { enabled: true }
     });
 
-    // Owl carousel
+
+    // Owl-carousel
+
     $('.site-main .about-area .owl-carousel').owlCarousel({
         loop: true,
         autoplay: true,
@@ -29,24 +35,23 @@ $(document).ready(function(){
             0: {
                 items: 1
             },
-            544:{
+            560: {
                 items: 2
             }
         }
     })
 
+    // sticky navigation menu
 
-    //sticky navigation menu
-    let nav_offset_top = $(".header_area").height() + 50;
+    let nav_offset_top = $('.header_area').height() + 50;
 
-    function navbarFixed(){
-        if($(".header_area").length){
-            $(window).scroll(function (){
+    function navbarFixed() {
+        if ($('.header_area').length) {
+            $(window).scroll(function () {
                 let scroll = $(window).scrollTop();
-
-                if(scroll >= nav_offset_top){
+                if (scroll >= nav_offset_top) {
                     $('.header_area .main-menu').addClass('navbar_fixed');
-                }else{
+                } else {
                     $('.header_area .main-menu').removeClass('navbar_fixed');
                 }
             })
@@ -54,4 +59,5 @@ $(document).ready(function(){
     }
 
     navbarFixed();
+
 });
