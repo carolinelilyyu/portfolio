8f97c60144210b805bd1980d5eb92b67a3ed9449
Caroline Yu <carolineyu@Carolines-MBP.askey.com>
2023-02-06 14:56:43 -0500

    viewable

---
+++ spa23tictactoep1/js/main.js
@@ -0,0 +1,26 @@
+function win(){
+    // in each row
+    // if columns 1,2,3 are of one type
+
+    // o  o  o
+    // c1 c2 c3
+    // c1 c2 c3
+
+
+    // in each column
+    // row 1,2,3 are of one type
+
+
+    // o c2 c3
+    // o c2 c3
+    // o c2 c3
+
+
+    // for all rows
+    // columns 1,2,3 are of one type
+
+    // o c2 c3
+    // c1 o c3
+    // c1 c2 o
+
+}
