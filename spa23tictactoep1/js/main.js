45187331ef328b91371623484aa1c7f82f199409
Caroline Yu <carolineyu@Carolines-MBP.askey.com>
2023-02-06 15:51:53 -0500

    no experience section, click images

--- spa23tictactoep1/js/main.js
+++ spa23tictactoep1/js/main.js
@@ -1,26 +1,34 @@
-function win(){
+$(document).ready(function () {
+    var arr =new Array(3)
+        for (i=0; i <3; i++)
+            arr[i]=new Array(3)
+
+    $("table tr").each(function(i, val){
+        $(this).find('td').each(function(j, val2){
+            console.log(j, val2)
+            arr[i][j] = parseInt($(this).attr('data-points'));
+        });
+    });
+
+    /// makes a 3x3 (2d array) grid of zeros 
+            
+    
+});
+
     // in each row
     // if columns 1,2,3 are of one type
-
     // o  o  o
     // c1 c2 c3
     // c1 c2 c3
 
-
     // in each column
     // row 1,2,3 are of one type
-
-
     // o c2 c3
     // o c2 c3
     // o c2 c3
-
 
     // for all rows
     // columns 1,2,3 are of one type
-
     // o c2 c3
     // c1 o c3
     // c1 c2 o
-
-}
