0f8b0e9f2715404d4ff7afbc04d5bc96c48ad200
Caroline Yu <carolineyu@Carolines-MBP.askey.com>
2023-03-01 15:57:33 -0500

    need to get rid of commas, cannot hit the same spot twice, answer to questions, prompt sunk/hit/miss

--- spa23_battleship/js/main.js
+++ spa23_battleship/js/main.js
@@ -1,10 +1,12 @@
 $(document).ready(function () {
         window.onload = function() {
             var turns = 20;
+            var shipSizes = [];
             buildBoard();
             var json = makeJson();
-            var shipCoords = getShipCoords(json);
-            placeShips(turns, shipCoords);
+            // changes shipSizes 
+            var ships = getShipCoords(json, shipSizes);
+            placeShips(turns, ships, shipSizes);
         }
 });
 
@@ -29,7 +31,7 @@ function makeJson(){
     }
 }
 
-function getShipCoords(json){
+function getShipCoords(json, shipSizes){
     // gathers the coordinates that have ships on them
     shipCoords = [];
     // read the json
@@ -38,12 +40,13 @@ function getShipCoords(json){
         for (var key in obj){
             var value = obj[key];
             var [row, column] = value["coords"]
+            shipSizes.push([value["name"], value["size"]]);
              // whether the ship is vertical or horizontal, gather the coordinates of the ship according its own size and orientation
             if(value["orientation"] == "vertical"){
-                shipCoords.push((Array(value["size"]).fill().map((_, index)=>[index+row, column])));
+                shipCoords.push((Array(value["size"]).fill().map((_, index)=>[index+row, column, value["name"]])));
             }
             else if(value["orientation"] == "horizontal"){
-                shipCoords.push((Array(value["size"]).fill().map((_, index)=>[row, column+index])));
+                shipCoords.push((Array(value["size"]).fill().map((_, index)=>[row, column+index, value["name"]])));
             }
         }
     }
@@ -68,24 +71,48 @@ function buildBoard(){
     battleshipBoard = document.getElementById("battleship-board").innerHTML = board.join('<br></br>');
 }
 
-function placeShips(turns, shipCoords){
+function placeShips(turns, ship, shipSizes){
     // make a click listener for each box
     document.querySelectorAll(".square").forEach(
         function(item){
             item.addEventListener("click", function(){
+                console.log(turns)
+                if(turns <= 0){
+                    alert("GAME OVER");
+                }
                 // get the clicked square's coordinates
                 var [clickRow, clickColumn] = [parseInt(item.getAttribute("id")), parseInt(item.getAttribute("name"))]
                 // compare the own coordinates to any of the coordinates with a ship on it. If true, it's a hit. If false, it's a miss
-                for (i in shipCoords){
-                    if(shipCoords[i].toString() == [clickRow, clickColumn].toString()){
-                        $(this).addClass('red');
+                for (i in ship){
+                    // each ship has details
+                    var shipDetails = ship[i]
+                    // each ship gives the coords for first and second elements
+                    var shipCoords = shipDetails.slice(0, 2);
+                    // the third element is the name of the ship
+                    var shipName = shipDetails[2];
+                    if(shipCoords.toString() == [clickRow, clickColumn].toString()){
+                        for(i in shipSizes){
+                            // if the ship belongs in the shipSizes array
+                            if(shipSizes[i].indexOf(shipName)>=0){
+                                // subtract one from the size because it's been visited
+                                shipSizes[i][1] -= 1;
+                                if(shipSizes[i][1] <= 0){
+                                    // indicates to user it blew up
+                                    alert("BLEW UP THIS SHIP");
+                                }
+                            }
+                        }
+                        // not returning, different ships might have the same coordinate so we'll need to keep iterating until all ships with
+                        // that clicked coordinate have their shipSize subtracted from.
+                        $(this).addClass('red full');
                         turns--;
-                        return true;
                     }
                 }
-                turns--;
-                $(this).addClass('grey');
-                return false;
+                // 
+                if($(this).hasClass("red") != true){
+                    turns--;
+                    $(this).addClass('grey full');
+                }
             });
         }
     )
