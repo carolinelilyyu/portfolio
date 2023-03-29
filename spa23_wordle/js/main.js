262780727dbd9e00eeac46bec8daba79ff6d9fe8
Caroline Yu <carolineyu@Carolines-MBP.askey.com>
2023-03-29 16:45:43 -0400

    bug in apple

--- spa23_wordle/js/main.js
+++ spa23_wordle/js/main.js
@@ -1,7 +1,9 @@
 $(document).ready(function () {
     const NUMBER_OF_GUESSES = 6
-    var wordle = new Wordle('ikppp', NUMBER_OF_GUESSES)
+    var wordle = new Wordle('apple', NUMBER_OF_GUESSES)
     wordle.initBoard();
+    wordle.listenForKeyboard();
+    wordle.keyboardEvent();
 });
 
 
@@ -11,9 +13,85 @@ class Wordle{
         this.NUMBER_OF_GUESSES = NUMBER_OF_GUESSES
         this.guessesRemaining = NUMBER_OF_GUESSES;
         this.answer = answer;
+        this.nextLetter = 0;
         this.usedLetters = [];
         this.answerArray = answer.split("")
+        this.guessArray = []
         this.displayColor = ['b', 'b', 'b', 'b', 'b']
+    }
+
+    listenForKeyboard(){
+        var board = document.getElementById("keyboard")
+
+        board.addEventListener("click", (e)=>{
+            var target = e.target
+            if (!target.classList.contains("key")) {
+                return
+            }
+            let letter = target.innerHTML
+    
+            if (letter === "del") {
+                letter = "Backspace"
+            } 
+            else if (letter === 'enter'){
+                letter = 'Enter'
+            }
+        
+            document.dispatchEvent(new KeyboardEvent("keyup", {"key": letter}))
+        })
+    }
+
+    keyboardEvent(){
+        // we are adding an event with key set to the letter that was from listenForKeyboard()
+        document.addEventListener("keyup", (e)=>{
+            if (this.guessesRemaining === 0) {
+                return
+            }
+
+            let key = e.key
+            if(key == 'Backspace'){
+                this.deleteLetter()
+            }
+            else if(key == 'Enter'){
+                if(this.nextLetter < 4){
+                    alert("Add more letters to your guess!")
+                }
+                else{
+                    console.log("lets guess")
+                    // console.log(this.guessArray)
+                    this.guess(this.guessArray)
+                }
+            }
+            else{
+                this.insertLetter(key)
+            }
+        })
+    }
+
+    deleteLetter(){
+        let row = document.getElementsByClassName("row")[6 - this.guessesRemaining]
+        let cell = row.children[this.nextLetter - 1]
+        cell.innerHTML = "";
+        cell.classList.remove('filled')
+        this.guessArray.pop()
+        this.nextLetter -= 1
+
+        // console.log(this.guessArray)
+    }
+
+    insertLetter(key){
+        if (this.nextLetter === 5) {
+            return
+        }
+        var pressedKey = key.toLowerCase()
+        let row = document.getElementsByClassName("row")[6 - this.guessesRemaining]
+        let cell = row.children[this.nextLetter]
+        cell.innerHTML = pressedKey
+        cell.classList.add('filled')
+        this.guessArray.push(pressedKey)
+        this.nextLetter += 1
+
+        // console.log(this.guessArray)
     }
 
     initBoard(){
@@ -33,14 +111,15 @@ class Wordle{
         }
     }
 
-    guess(guessWord){
-        var guessArray = guessWord.split("")
+    // instead of splitting the word, give the whole array of letters
+    guess(guessArray){
         var answerArray = this.answerArray
         var displayColor = this.displayColor
         // green check
         guessArray.forEach(function(guessElement, guessIndex){
             var answerIndex = answerArray.indexOf(guessElement)
-            if(answerIndex == guessIndex){
+            console.log(answerIndex)
+            if(answerIndex === guessIndex){
                 answerArray[answerIndex] = null
                 guessArray[guessIndex] = null
                 displayColor[answerIndex] = 'g'
