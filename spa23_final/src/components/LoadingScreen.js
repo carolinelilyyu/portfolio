e3ec9e590524bb714d47d3d2275fdb52f4314dec
Caroline Yu <carolineyu@Carolines-MBP.askey.com>
2023-06-26 13:44:31 -0400

    tier-list

--- spa23_final/src/components/LoadingScreen.js
+++
@@ -1,7 +0,0 @@
-export default function LoadingScreen(props) {
-    return (
-        <div>
-            This is the loading screen.
-        </div>
-    );
-}
--- spa23_final/src/components/NotFound.js
+++
@@ -1,7 +0,0 @@
-export default function NotFound(){
-    return (
-    <div className = "page">
-        <h1>Oh no! There's no such page</h1>
-    </div>
-    );
-}
